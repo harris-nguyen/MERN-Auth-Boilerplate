ec7d2988d92e46a669d2e5dd5b04db6ff465f4b4
Harris Nguyen <harrisq.nguyen@gmail.com>
2020-05-21 14:41:17 -0700

    connected to mongodb

--- server/server.js
+++ server/server.js
@@ -1,12 +1,46 @@
 const express = require("express");
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require('dotenv').config()
+
 
 const app = express();
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("mongoDB connected")
+  })
+  .catch(err => console.log('mongoDB error', err))
 
 // import routes
 const authRoutes = require("./routes/auth");
 
-// middleware
+//  app middlewares
+
+app.use(morgan('dev')) // status code ex in terminal: GET /api/signup 304 4.194 ms - -
+app.use(bodyParser.json())
+
+//app.use(cors()) // allow all origins
+if ((process.env.NODE_ENV = "development")) {
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
+
+// route middleware
 app.use("/api", authRoutes);
 
-const PORT = process.env.port || 8000;
-app.listen(PORT, console.log(`server listening to port ${PORT}`));
+
+const port = process.env.PORT || 8000;
+app.listen(port, console.log(`server listening to port ${port}`));
