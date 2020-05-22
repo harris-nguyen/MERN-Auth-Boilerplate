11d0aff5ce2b99139c4fedace3fc1bedbc49f2cd
Harris Nguyen <harrisq.nguyen@gmail.com>
2020-05-22 11:32:17 -0700

    user can sign in

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -10,49 +10,145 @@ const sgMail = require("@sendgrid/mail");
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-// below is to help avoid spam. We will send user a user email confirmation with web token
 exports.signup = (req, res) => {
-  const { name, email, password } = req.body;
+    // console.log('REQ BODY ON SIGNUP', req.body);
+    const { name, email, password } = req.body;
 
+    User.findOne({ email }).exec((err, user) => {
+        if (user) {
+            return res.status(400).json({
+                error: 'Email is taken'
+            });
+        }
+    });
+
+    let newUser = new User({ name, email, password });
+
+    newUser.save((err, success) => {
+        if (err) {
+            console.log('SIGNUP ERROR', err);
+            return res.status(400).json({
+                error: err
+            });
+        }
+        res.json({
+            message: 'Signup success! Please signin'
+        });
+    });
+};
+
+exports.accountActivation = (req, res) => {
+  const { token } = req.body;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, function (err, decoded) {
+      if (err) {
+        console.log("JWT VERIFY IN ACCOUNT ACTIVATION ERROR", err);
+        return res.status(401).json({
+          error: "Expired link. Signup again",
+        });
+      }
+
+      const { name, email, password } = jwt.decode(token);
+
+      const user = new User({ name, email, password });
+
+      user.save((err, user) => {
+        if (err) {
+          console.log("SAVE USER IN ACCOUNT ACTIVATION ERROR", err);
+          return res.status(401).json({
+            error: "Error saving user in database. Try signup again",
+          });
+        }
+        return res.json({
+          message: "Signup success. Please signin.",
+        });
+      });
+    });
+  } else {
+    return res.json({
+      message: "Something went wrong. Try again.",
+    });
+  }
+};
+
+exports.signin = (req, res) => {
+  const { email, password } = req.body;
+  // check if user exist
   User.findOne({ email }).exec((err, user) => {
-    if (user) {
+    if (err || !user) {
       return res.status(400).json({
-        error: "Email is taken",
+        error: "User with that email does not exist. Please signup",
       });
     }
-
-    const token = jwt.sign(
-      { name, email, password },
-      process.env.JWT_ACCOUNT_ACTIVATION,
-      { expiresIn: "10m" }
-    );
-
-    const emailData = {
-      from: process.env.EMAIL_FROM,
-      to: email,
-      subject: `Account activation link`,
-      html: `
-                <h1>Please use the following link to activate your account</h1>
-                <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
-                <hr />
-                <p>This email may contain sensetive information</p>
-                <p>${process.env.CLIENT_URL}</p>
-            `,
-    };
-
-    sgMail
-      .send(emailData)
-      .then((sent) => {
-        // console.log('SIGNUP EMAIL SENT', sent)
-        return res.json({
-          message: `Email has been sent to ${email}. Follow the instruction to activate your account`,
-        });
-      })
-      .catch((err) => {
-        // console.log('SIGNUP EMAIL SENT ERROR', err)
-        return res.json({
-          message: err.message,
-        });
+    // authenticate
+    if (!user.authenticate(password)) {
+      return res.status(400).json({
+        error: "Email and password do not match",
       });
+    }
+    // generate a token and send to client
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    const { _id, name, email, role } = user;
+
+    return res.json({
+      token,
+      user: { _id, name, email, role },
+    });
   });
 };
+
+
+
+
+
+
+
+// below is to help avoid spam. We will send user a user email confirmation with web token
+// exports.signup = (req, res) => {
+//   const { name, email, password } = req.body;
+
+//   User.findOne({ email }).exec((err, user) => {
+//     if (user) {
+//       return res.status(400).json({
+//         error: "Email is taken",
+//       });
+//     }
+
+//     const token = jwt.sign(
+//       { name, email, password },
+//       process.env.JWT_ACCOUNT_ACTIVATION,
+//       { expiresIn: "10m" }
+//     );
+
+//     const emailData = {
+//       from: process.env.EMAIL_FROM,
+//       to: email,
+//       subject: `Account activation link`,
+//       html: `
+//                 <h1>Please use the following link to activate your account</h1>
+//                 <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
+//                 <hr />
+//                 <p>This email may contain sensetive information</p>
+//                 <p>${process.env.CLIENT_URL}</p>
+//             `,
+//     };
+
+//     sgMail
+//       .send(emailData)
+//       .then((sent) => {
+//         // console.log('SIGNUP EMAIL SENT', sent)
+//         return res.json({
+//           message: `Email has been sent to ${email}. Follow the instruction to activate your account`,
+//         });
+//       })
+//       .catch((err) => {
+//         // console.log('SIGNUP EMAIL SENT ERROR', err)
+//         return res.json({
+//           message: err.message,
+//         });
+//       });
+//   });
+// };
