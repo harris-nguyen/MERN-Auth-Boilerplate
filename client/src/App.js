8a5b8d600311b541c70156f008344d5ba88c1691
Harris Nguyen <harrisq.nguyen@gmail.com>
2020-05-22 16:51:06 -0700

    updating

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,10 @@ import Layout from './core/layout'
 const App = () => {
   return (
     <Layout>
-      <h1>Hello React</h1>
+      <div className="col-md-6 offset-md-3">
+        <h1 className="p-5 text-center">WELCOME TO YOUR APP</h1>
+        <br />
+      </div>
     </Layout>
   );
 }
