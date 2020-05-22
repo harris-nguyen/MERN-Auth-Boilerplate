b3804fa72d6f3100c803e41692ceb088716e491a
Harris Nguyen <harrisq.nguyen@gmail.com>
2020-05-22 12:56:24 -0700

    front end react nav bar react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Layout from './core/layout'
 
 const App = () => {
-  return(
-    <div  className=''>Hello React</div>
-  )
+  return (
+    <Layout>
+      <h1>Hello React</h1>
+    </Layout>
+  );
 }
 
 export default App;
---
+++ client/src/core/layout.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react'
+import {Link} from 'react-router-dom' // page does not reload when clicked on a nav bar link for example
+
+const Layout = ({children}) => {
+  const nav = () => (
+    <ul className="nav nav-tabs bg-primary">
+      <li className="nav-item">
+        <Link to="/" className="text-light nav-link">
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/signup" className="text-light nav-link">
+          Sign up
+        </Link>
+      </li>
+    </ul>
+  );
+
+  return(
+    <Fragment>
+      {nav()}
+      <div className='container'>
+          {children}
+      </div>
+    </Fragment>
+  )
+}
+
+export default Layout
