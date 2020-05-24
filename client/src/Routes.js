7acd579bb328009aa0e46c53fdaa1f982f34dcef
Harris Nguyen <harrisq.nguyen@gmail.com>
2020-05-24 13:19:34 -0700

    user can log in and have private page

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import App from './App'
 import Signup from './auth/Signup'
 import Signin from './auth/Signin'
+import Private from './core/Private'
+import PrivateRoute from './auth/PrivateRoute'
 
 
 const Routes = () => {
@@ -13,6 +15,7 @@ const Routes = () => {
           <Route path="/" exact component={App} />
           <Route path="/signup" exact component={Signup} />
           <Route path="/signin" exact component={Signin} />
+          <PrivateRoute path="/private" exact component={Private} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ client/src/auth/PrivateRoute.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {Route, Redirect} from 'react-router-dom'
+import {isAuth} from './Helpers'
+
+const PrivateRoute = ({component: Component, ...rest}) =>(
+  <Route {...rest} render= {
+    props => isAuth() ? <Component {...props} /> : <Redirect to={{
+      pathname: '/signin',
+      state: {from: props.location}
+    }} />
+  }></Route>
+)
+
+export default PrivateRoute
